b2bf6797fdb2f342e5efbf37b3ac7f44d762c407
CodyGividen <codygividen5@gmail.com>
2018-03-16 15:35:33 -0400

    Assessment

--- app/src/main/java/com/example/rodneytressler/week1assessment/MainActivity.java
+++ app/src/main/java/com/example/rodneytressler/week1assessment/MainActivity.java
@@ -1,18 +1,43 @@
 package com.example.rodneytressler.week1assessment;
 
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button submitButton;
+    private TextView textInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
         setContentView(R.layout.activity_main);
+        submitButton = findViewById(R.id.button_submit);
+        textInput = findViewById(R.id.text_input_edit_text);
+        onClickListener();
+    }
+
+    protected void onClickListener(){
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(onClick(), "Android is fun!!!!!!", Toast.LENGTH_LONG).show();
+                textInput.setText("");
+                startmenu();
+            }
+        });
+    }
+    private void startmenu(){
+        Intent android = new Intent(this, android_is_fun.class);
+        startActivity(android);
+        finish();
     }
 }
---
+++ app/src/main/java/com/example/rodneytressler/week1assessment/android_is_fun.java
@@ -0,0 +1,25 @@
+package com.example.rodneytressler.week1assessment;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+public class android_is_fun extends AppCompatActivity {
+    //private TextView love;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_android_is_fun);
+        //love = findViewById(R.id.love_textview);
+
+    }
+    private void nameLove(){
+
+    }
+
+}
